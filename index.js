a7b9c99805252c0f4387fb547eaca833fa7397e5
Marcel Schoombee <marcelsch01@yahoo.com>
2024-04-12 09:31:53 +0200

    currency changed

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
             <span>${data.name}</span>
         `
         document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
+            <p>🎯: R${data.market_data.current_price.rnd}</p>
+            <p>👆: R${data.market_data.high_24h.rnd}</p>
+            <p>👇: R${data.market_data.low_24h.rnd}</p>
         `
     })
     .catch(err => console.error(err))
