9d623fd684d2ca7a738e9061e554ee27251c0c1a
Marcel Schoombee <marcelsch01@yahoo.com>
2024-04-15 11:06:12 +0200

    Comments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature") // fetches the API endpoint
+    .then(res => res.json()) //converts the response to json method
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.body.style.backgroundImage = `url(${data.urls.regular})` // fetches the image from the url with regular-size
+		document.getElementById("author").textContent = `By: ${data.user.name}` // updates the auther name in a element id 
     })
-    .catch(err => {
+    .catch(err => {                         // this sets a default image if a error happens
         // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
@@ -19,11 +19,11 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         return res.json()
     })
     .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
+        document.getElementById("crypto-top").innerHTML = ` // shows the dogecoin logo and name in a element 'crypto-top'
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `
-        document.getElementById("crypto").innerHTML += `
+        document.getElementById("crypto").innerHTML += ` // shows the market data in a element called 'crypto'
             <p>🎯: R${data.market_data.current_price.zar}</p>
             <p>👆: R${data.market_data.high_24h.zar}</p>
             <p>👇: R${data.market_data.low_24h.zar}</p>
@@ -31,22 +31,22 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     })
     .catch(err => console.error(err))
 
-function getCurrentTime() {
-    const date = new Date()
+function getCurrentTime() { // function is defined to get current time
+    const date = new Date() // its formatted to display only the time portion using 'toLocaleTimeString' 'short'
     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
-}
+}                           
 
-setInterval(getCurrentTime, 1000)
+setInterval(getCurrentTime, 1000)       // and its called every second to keep time up to date
 
-navigator.geolocation.getCurrentPosition(position => {
+navigator.geolocation.getCurrentPosition(position => { // obtain users current position 
     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
         .then(res => {
             if (!res.ok) {
-                throw Error("Weather data not available")
+                throw Error("Weather data not available") // if there is a error
             }
-            return res.json()
+            return res.json() // returns the data as json
         })
-        .then(data => {
+        .then(data => {             // returns the weather icon, rounded off temp and city name
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
